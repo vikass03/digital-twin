e3ecd7f7893027aa861da0f93a90b65fd2399ab4
Vikas Seth <seth_vikas@yahoo.com>
2026-01-07 12:55:37 +0100

    Add CI/CD with GitHub Actions, S3 backend, and updated scripts

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,10 +25,10 @@ resource "aws_s3_bucket" "memory" {
 resource "aws_s3_bucket_public_access_block" "memory" {
   bucket = aws_s3_bucket.memory.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_ownership_controls" "memory" {
